f0f41bdff6bcfa9107c6e45addfc5754ef7d7e08
Vitaliy123 <cpc1251@mail.ru>
2022-06-29 20:12:32 +0300

    2.2. Модель данных - Post. [#285211]

---
+++ src/main/java/ru/job4j/grabber/Post.java
@@ -0,0 +1,41 @@
+package ru.job4j.grabber;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class Post {
+    private int id;
+    private String title;
+    private String link;
+    private String description;
+    private LocalDateTime created;
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Post{");
+        sb.append("id=").append(id);
+        sb.append(", title='").append(title).append('\'');
+        sb.append(", link='").append(link).append('\'');
+        sb.append(", description='").append(description).append('\'');
+        sb.append(", created=").append(created);
+        sb.append('}');
+        return sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Post post = (Post) o;
+        return id == post.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
